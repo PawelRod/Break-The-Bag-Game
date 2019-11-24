3ecb82a5001603100105b1014682df347235ee03
Unknown <rodziewiczpp@gmail.com>
2019-11-24 19:42:17 +0100

    first commit

---
+++ src/components/Game.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="game">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
